b6cbd26aeb3b1c4c0ebdb5e89a1123413b359a32
skywrt <156793219+skywrt@users.noreply.github.com>
2025-07-11 12:13:12 +0800

    Create dynamic.py

---
+++ dynamic.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import re
+import datetime
+import requests
+import threading
+from typing import Set
+from fetch import raw2fastly, session, LOCAL
+
+
+
+def sharkdoor():
+    res_json = session.get(datetime.datetime.now().strftime(
+        'https://api.github.com/repos/sharkDoor/vpn-free-nodes/contents/node-list/%Y-%m?ref=master')).json()
+    res = session.get(raw2fastly(res_json[-1]['download_url']))
+    nodes: Set[str] = set()
+    for line in res.text.split('\n'):
+        if '://' in line:
+            nodes.add(line.split('|')[-2])
+    return nodes
+
+def changfengoss():
+    # Unused
+    res = session.get(datetime.datetime.now().strftime(
+        "https://api.github.com/repos/changfengoss/pub/contents/data/%Y_%m_%d?ref=main")).json()
+    return [_['download_url'] for _ in res]
+
+
+def w1770946466():
+    if LOCAL: return
+    res = session.get(raw2fastly("https://raw.githubusercontent.com/w1770946466/Auto_proxy/main/README.md")).text
+    subs: Set[str] = set()
+    for line in res.strip().split('\n'):
+        if line.startswith("`http"):
+            sub = line.strip().strip('`')
+            if not sub.startswith("https://raw.githubusercontent.com"):
+                subs.add(sub)
+    return subs
+
+def peasoft():
+    return session.get("https://gist.githubusercontent.com/peasoft/8a0613b7a2be881d1b793a6bb7536281/raw/417c1d6a75a53d6c197448762e7c97852d34787f/-").text
+
+AUTOURLS = []
+AUTOFETCH = [peasoft]
+
+if __name__ == '__main__':
+    print("URL 抓取："+', '.join([_.__name__ for _ in AUTOURLS]))
+    print("内容抓取："+', '.join([_.__name__ for _ in AUTOFETCH]))
+    import code
+    code.interact(banner='', exitmsg='', local=globals())
